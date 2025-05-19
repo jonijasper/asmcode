497fa9df5882b4506ab16bf6533c1c489623dae1
jonijasper <jasper.alakotila@protonmail.com>
2025-05-19 20:14:34 +0300

    helloworld instructions

--- helloworld/hello.asm
+++ helloworld/hello.asm
@@ -2,7 +2,14 @@ section .text
     global _start
 
 _start:     ;entry point
-    ;instructions
+    mov edx,len
+    mov ecx,msg
+    mov ebx,1   ;stdout
+    mov eax,4   ;sys_write
+    int 0x80
+
+    mov eax,1   ;sys_exit
+    int 0x80
 
 section .data
 msg db 'Hello, world!',0xa
