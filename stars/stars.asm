6cbb860df5836118972929f6c2d90d2ded971ce0
jonijasper <jasper.alakotila@protonmail.com>
2025-05-19 20:48:26 +0300

    print 9 stars, instructions

--- stars/stars.asm
+++ stars/stars.asm
@@ -2,7 +2,24 @@ section .text
     global _start
 
 _start:
-    ;instructions
+    ;print msg
+    mov eax,4 ;sys_write
+    mov ebx,1 ;stdout
+    mov ecx,msg
+    mov edx,len
+    int 0x80
+
+    ;print stars
+    mov eax,4
+    mov ebx,1
+    mov ecx,stars
+    mov edx,9 ;stars string is 9 bytes
+    int 0x80
+
+    ;exit
+    mov eax,1
+    mov ebx,0
+    int 0x80
 
 section .data
 msg db 'Displaying 9 stars',0xa
